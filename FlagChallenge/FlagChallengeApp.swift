7c8f220df3e466f8dd9cff2ea83ef68d74c4e7ac
Ostap Artym <otapik125@gmail.com>
2025-02-18 17:55:39 +0200

    Initial Commit

---
+++ FlagChallenge/FlagChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagChallengeApp.swift
+//  FlagChallenge
+//
+//  Created by Ostap Artym on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
